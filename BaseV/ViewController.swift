2021fec97877af944903ab44e2cf9240b1ba6dc7
Varun Kumar <varun.kumar@tothenew.com>
2020-05-04 23:55:14 +0530

    Initial Commit

---
+++ BaseV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseV
+//
+//  Created by Varun Kumar on 04/05/20.
+//  Copyright © 2020 Varun Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BaseVTests/BaseVTests.swift
@@ -0,0 +1,34 @@
+//
+//  BaseVTests.swift
+//  BaseVTests
+//
+//  Created by Varun Kumar on 04/05/20.
+//  Copyright © 2020 Varun Kumar. All rights reserved.
+//
+
+import XCTest
+@testable import BaseV
+
+class BaseVTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
